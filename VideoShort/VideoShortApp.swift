620201ba531d15abc88c2b5d8e753c052ef3aada
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-27 15:35:01 +0600

    Initial Commit

---
+++ VideoShort/VideoShortApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoShortApp.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoShortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
