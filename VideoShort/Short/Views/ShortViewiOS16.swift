7392e1b1d845072cb499147883140f47ea311bbe
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-27 15:53:41 +0600

    add Seperate view to manage short videos for ios 16 and 17+ versions

---
+++ VideoShort/Short/Views/ShortViewiOS16.swift
@@ -0,0 +1,18 @@
+//
+//  ShortViewiOS16.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import SwiftUI
+
+struct ShortViewiOS16: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShortViewiOS16()
+}
---
+++ VideoShort/Short/Views/ShortViewiOS17Plus.swift
@@ -0,0 +1,18 @@
+//
+//  ShortView.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import SwiftUI
+
+struct ShortViewiOS17Plus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShortViewiOS17Plus()
+}
