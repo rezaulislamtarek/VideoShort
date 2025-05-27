7d922f57efe0ce6162ccee886e00ed91759340ab
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-27 16:39:47 +0600

    Call ShortsViewiOS16

---
+++ VideoShort/Short/Views/CustomVideoPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  CustomVideoPlayer.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import SwiftUI
+
+struct CustomVideoPlayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomVideoPlayer()
+}
---
+++ VideoShort/Short/Views/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
