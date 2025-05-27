96fa96c9e094b0406d517192da2938f805acf245
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-27 18:12:52 +0600

    Clean and refactor FeedCell

--- VideoShort/Short/Views/FeedCell.swift
+++ VideoShort/Short/Views/FeedCell.swift
@@ -26,111 +26,36 @@ struct FeedCell: View {
     var body: some View {
         ZStack {
             
-            KFImage(URL(string: post.thumbnailUrl))
-                .resizable()
-                .scaledToFit()
-                //.opacity(currentVisibleId == post.id ? 0 : 1)
-                .animation(.easeInOut(duration: 0.3), value: currentVisibleId)
+            thumbNailSection
             
             if currentVisibleId == post.id {
                 CustomVideoPlayer(player: player, isBuffering: $isBuffering)
                     .transition(.opacity)
                     .animation(.easeInOut(duration: 0.3), value: currentVisibleId)
+                    
             }
             
             if isBuffering {
                 ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                    //.scaleEffect(1)
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    .scaleEffect(1.5)
             }
+            
+            Color.clear
+                .background(
+                    LinearGradient(colors: [Color.black.opacity(0.7), Color.black.opacity(0.5), Color.clear,Color.clear,Color.clear, Color.clear, Color.black.opacity(0.5), Color.black.opacity(0.7)], startPoint: .top, endPoint: .bottom)
+                )
             
             VStack {
                 Spacer()
                 
                 HStack(alignment: .bottom) {
-                    VStack(alignment: .leading) {
-                        Text("@test.user")
-                            .fontWeight(.semibold)
-                        
-                        Text("Rocket ship preparing to take off")
-                            .fontWeight(.semibold)
-                    }.font(.subheadline)
-                        .foregroundStyle(.white)
+                    shortInfoSection
                     
                     Spacer()
                     
-                    VStack(spacing: 28) {
-                        
-                        Circle()
-                            .frame(width: 48, height: 48)
-                            .foregroundStyle(.gray)
-                        
-                        Button {
-                            
-                        } label: {
-                            VStack {
-                                Image(systemName: "heart.fill")
-                                    .resizable()
-                                    .frame(width: 28,height: 28)
-                                    .foregroundStyle(.white)
-                                
-                                Text("12")
-                                    .font(.footnote)
-                                    .foregroundStyle(.white)
-                                    .bold()
-                            }
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            VStack {
-                                Image(systemName: "ellipsis.bubble.fill")
-                                    .resizable()
-                                    .frame(width: 28,height: 28)
-                                    .foregroundStyle(.white)
-                                
-                                Text("27")
-                                    .font(.footnote)
-                                    .foregroundStyle(.white)
-                                    .bold()
-                            }
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            VStack {
-                                Image(systemName: "bookmark.fill")
-                                    .resizable()
-                                    .frame(width: 22,height: 28)
-                                    .foregroundStyle(.white)
-                                
-                                Text("2")
-                                    .font(.footnote)
-                                    .foregroundStyle(.white)
-                                    .bold()
-                            }
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            VStack {
-                                Image(systemName: "arrowshape.turn.up.right.fill")
-                                    .resizable()
-                                    .frame(width: 28,height: 28)
-                                    .foregroundStyle(.white)
-                                
-                                
-                                Text("30")
-                                    .font(.footnote)
-                                    .foregroundStyle(.white)
-                                    .bold()
-                            }
-                            
-                        }
-                    }
+                    shortsRightActionSection
+                    
                 }.padding(.bottom, 80)
                 
             }.padding()
@@ -146,6 +71,110 @@ struct FeedCell: View {
                 break
             }
         }
+    }
+}
+
+extension FeedCell {
+    private var shortInfoSection: some View {
+        VStack(alignment: .leading) {
+            HStack{
+                KFImage(URL(string: post.userProfilePictureUrl))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                Text("@\(post.username)")
+                    .fontWeight(.semibold)
+            }
+            
+            Text("\(post.caption)")
+                .fontWeight(.semibold)
+        }.font(.subheadline)
+            .foregroundStyle(.white)
+    }
+    
+    
+    private var shortsRightActionSection: some View {
+        VStack(spacing: 28) {
+            
+            Circle()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(.gray)
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .frame(width: 28,height: 28)
+                        .foregroundStyle(.white)
+                    
+                    Text("\(post.likeCount)")
+                        .font(.footnote)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(systemName: "ellipsis.bubble.fill")
+                        .resizable()
+                        .frame(width: 28,height: 28)
+                        .foregroundStyle(.white)
+                    
+                    Text("\(post.commentCount)")
+                        .font(.footnote)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(systemName: "bookmark.fill")
+                        .resizable()
+                        .frame(width: 22,height: 28)
+                        .foregroundStyle(.white)
+                    
+                    Text("\(Int(post.likeCount / 2))")
+                        .font(.footnote)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(systemName: "arrowshape.turn.up.right.fill")
+                        .resizable()
+                        .frame(width: 28,height: 28)
+                        .foregroundStyle(.white)
+                    
+                    
+                    Text("\(Int(post.likeCount / 3))")
+                        .font(.footnote)
+                        .foregroundStyle(.white)
+                        .bold()
+                }
+                
+            }
+        }
+    }
+    
+    private var thumbNailSection : some View {
+        KFImage(URL(string: post.thumbnailUrl))
+            .resizable()
+            .scaledToFit()
+            //.opacity(currentVisibleId == post.id ? 0 : 1)
+            .animation(.easeInOut(duration: 0.3), value: currentVisibleId)
     }
 }
 
