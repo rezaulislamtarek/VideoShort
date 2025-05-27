27a7fb7cd0cde33ca80e3b27876f72f549e16ff3
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-27 15:52:58 +0600

    add videmodel for manage posts

---
+++ VideoShort/Short/ViewModel/ShortViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ShortViewModel.swift
+//  VideoShort
+//
+//  Created by Rezaul Islam on 5/27/25.
+//
+
+import Foundation
+ 
+final class ShortViewModel : ObservableObject  {
+    @Published var posts = [Post]()
+    
+    init() {
+        fetchPosts()
+    }
+    
+    private func fetchPosts() {
+        posts = Post.posts
+    }
+}
